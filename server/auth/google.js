4e428d8d2cac23a14a6d4a545edcb5dc95ec6192
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-06-08 21:35:06 -0400

    testing google login

--- server/auth/google.js
+++ server/auth/google.js
@@ -20,7 +20,7 @@ module.exports = router
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
-  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+  console.log('Google client ID / secrt not found. Skipping Google OAuth.')
 
 } else {
 
