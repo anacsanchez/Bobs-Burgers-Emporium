89b03c92cd3713b2b39cb251681097c0428f86a0
Ana <ana@Anas-MBP.authority>
2019-04-17 22:24:33 -0400

    changed database connection url

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const compression = require('compression')
 const session = require('express-session')
+require('../secrets')
 const passport = require('passport')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const db = require('./db')
@@ -11,8 +12,6 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 module.exports = app
-
-if (process.env.NODE_ENV !== 'production') require('../secrets')
 
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id))
