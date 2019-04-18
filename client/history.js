76574e124f7a62882bf0f44dcdf46b8dc196384f
Ana C Sanchez <ana@anacsanchez.com>
2019-04-18 14:55:55 -0400

    added basename to history object for router

--- client/history.js
+++ client/history.js
@@ -1,6 +1,8 @@
 import createHistory from 'history/createBrowserHistory'
 import createMemoryHistory from 'history/createMemoryHistory'
 
-const history = process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory()
+const history = process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory({ basename: '/burgers-emporium'})
+
+// history.listen(location => { console.log(location.pathname) })
 
 export default history
